7b9ace65585060da8e89292e1418a8a2c053cdab
Annu9111 <aksoni911190@gmail.com>
2026-02-11 17:44:41 +0530

    script

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const password = document.getElementById("password");
 const strength = document.getElementById("strenght");
 const message = document.getElementById("message");
 
+password.addEventListener("input",()=>{
+  const value=password.value;
+  let score = 0;
+
+  if (value.length >= 6) score++;
+  if (/[A-Z]/.test(value)) score++;
+  if (/[0-9]/.test(value)) score++;
+  if (/[^A-Za-z0-9]/.test(value)) score++;
+})
